7f562923bcb5ac088affb592ae2dfa3844ec96f7
YorittPXL <12101177@student.pxl.be>
2023-05-09 12:44:26 +0200

    Checkout met dynamische prijzen gemaakt met Javascript

---
+++ checkout.js
@@ -0,0 +1,39 @@
+// Selectie van button (kleur verandert)
+let selectedButton = null;
+
+document.getElementById("oneYearBtn").addEventListener("click", function () {
+    selectedButton = "oneYearBtn";
+    this.style.backgroundColor = "#F9CB33";
+    document.getElementById("oneMonthBtn").style.backgroundColor = "white"
+});
+
+document.getElementById("oneMonthBtn").addEventListener("click", function () {
+    selectedButton = "oneMonthBtn";
+    this.style.backgroundColor = "#F9CB33";
+    document.getElementById("oneYearBtn").style.backgroundColor = "white"
+});
+
+
+// Dynamische prijs dmv. klikken op knoppen
+
+// Vind de knoppen en de h3 tags
+let oneYearBtn = document.getElementById("oneYearBtn");
+let oneMonthBtn = document.getElementById("oneMonthBtn");
+let abonnementPrijs1 = document.getElementById("abonnementPrijs1");
+let abonnementPrijs2 = document.getElementById("abonnementPrijs2");
+
+// Hier word de tekst in het h3 element gezet.
+abonnementPrijs1.textContent = "€ 99,90";
+abonnementPrijs2.textContent = "Betaal €99,90";
+
+// Voeg een click event listener toe aan de knoppen zodat prijzen veranderen
+oneYearBtn.addEventListener("click", () => {
+    abonnementPrijs1.textContent = "€ 99,90";
+    abonnementPrijs2.textContent = "Betaal €99,90";
+});
+
+oneMonthBtn.addEventListener("click", () => {
+    abonnementPrijs1.textContent = "€ 9,90";
+    abonnementPrijs2.textContent = "Betaal €9,90";
+});
+
---
+++ checkoutmonth.js
@@ -0,0 +1,39 @@
+// Selectie van button (kleur verandert)
+let selectedButton2 = null;
+
+document.getElementById("oneYearBtn2").addEventListener("click", function() {
+    selectedButton2 = "oneYearBtn2";
+    this.style.backgroundColor = "#F9CB33";
+    document.getElementById("oneMonthBtn2").style.backgroundColor = "white"
+});
+
+document.getElementById("oneMonthBtn2").addEventListener("click", function() {
+    selectedButton2 = "oneMonthBtn2";
+    this.style.backgroundColor = "#F9CB33";
+    document.getElementById("oneYearBtn2").style.backgroundColor = "white"});
+
+
+
+// Dynamische prijs dmv. klikken op knoppen
+
+// Vind de knoppen en de h3 tags
+let oneYearBtn2 = document.getElementById("oneYearBtn2");
+let oneMonthBtn2 = document.getElementById("oneMonthBtn2");
+let abonnementPrijs3 = document.getElementById("abonnementPrijs3");
+let abonnementPrijs4 = document.getElementById("abonnementPrijs4");
+
+// Hier word de tekst in het h3 element gezet.
+abonnementPrijs3.textContent = "€ 9,90";
+abonnementPrijs4.textContent = "Betaal €9,90";
+
+// Voeg een click event listener toe aan de knoppen zodat prijzen veranderen
+oneYearBtn2.addEventListener("click", () => {
+    abonnementPrijs3.textContent = "€ 9,90";
+    abonnementPrijs4.textContent = "Betaal €9,90";
+});
+
+oneMonthBtn2.addEventListener("click", () => {
+    abonnementPrijs3.textContent = "€ 99,90";
+    abonnementPrijs4.textContent = "Betaal €99,90";
+});
+
