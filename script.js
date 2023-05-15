aeacda3362b76c0b71e0332a9ec17abbb6b3ce06
YorittPXL <12101177@student.pxl.be>
2023-05-15 16:49:23 +0200

    Navigatiebar met hamburger menu gemaakt + toegevoegd aan alle pagina's.
    Footer sticky bottom op niet scrollbare pagina's.

---
+++ script.js
@@ -0,0 +1,6 @@
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+navbarToggler.addEventListener('click', () => {
+    navbarCollapse.classList.toggle('show');
+});
